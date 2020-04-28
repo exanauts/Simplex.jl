b08cda999d9785b6071d8c8090691b607f65a4d5
Kibaek Kim <kimk@anl.gov>
2020-04-28 10:40:56 -0500

    fixed runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,8 @@ using SparseArrays
 using LinearAlgebra
 using Test
 
-# netlib = "afiro.mps"
-# Simplex.GLPK_solve(netlib)
+netlib = "afiro.mps"
 c, xl, xu, bl, bu, A = Simplex.GLPK_read_mps(netlib)
-# @show c, xlb, xub, l, u, A
 
 lp = Simplex.LpData(c, xl, xu, A, bl, bu, Array)
-# Simplex.summary(lp)
-# Simplex.run(lp)
 Simplex.run(lp, pivot_rule = Simplex.PIVOT_DANTZIG)
