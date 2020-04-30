29cf155322d484649ecc8c7e514e9933fa91bf5e
Kibaek Kim <kibaekkim@login4.mcs.anl.gov>
2020-04-30 14:50:56 -0500

    fixed runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,53 +6,52 @@ using Test
 
 # CuArrays.allowscalar(false)
 
+instances=[
+    "afiro", 
+    "adlittle",
+    "sc50a",
+    "sc50b",
+    "sc105",
+    "sc205"
+]
+phaseone_methods = [
+    Simplex.PhaseOne.ARTIFICIAL, 
+    Simplex.PhaseOne.CPLEX,
+]
 pivot_rules = [
     Simplex.Bland,
     Simplex.Steepest,
     Simplex.Dantzig,
 ]
 
+function run_netlib_instance(netlib, use_gpu, method, pivot)::Simplex.Status
+    c, xl, xu, bl, bu, A = Simplex.GLPK_read_mps(netlib)
+    lp = Simplex.LpData(c, xl, xu, A, bl, bu)
+    Simplex.run(lp, 
+        gpu = use_gpu,
+        phase_one_method = method,
+        pivot_rule = pivot)
+    return lp.status
+end
+
 @testset "netlib instances" begin
-    instances=[
-        "afiro", 
-        "adlittle",
-        "sc50a",
-        "sc50b",
-        "sc105",
-        "sc205"
-    ]
     for use_gpu in [false, true]
         arch = use_gpu ? "GPU" : "CPU"
-
         @testset "$arch" begin
             for i in instances
-                netlib = "../netlib/$i.mps"
-
                 @testset "$(i)" begin
-                    for method in [Simplex.PhaseOne.ARTIFICIAL, Simplex.PhaseOne.CPLEX]
-                
+                    for method in phaseone_methods
                         @testset "$(Symbol(method))" begin
-
                             for pivot in pivot_rules
-
                                 @testset "$(Symbol(pivot))" begin
-                                    c, xl, xu, bl, bu, A = Simplex.GLPK_read_mps(netlib)
-                                    lp = Simplex.LpData(c, xl, xu, A, bl, bu)
-                                    Simplex.run(lp, 
-                                        gpu = false,
-                                        phase_one_method = method,
-                                        pivot_rule = pivot)
-                                    @test lp.status == Simplex.Optimal
-                                    lp = nothing
+                                    netlib = "../netlib/$i.mps"
+                                    @test Simplex.Optimal == run_netlib_instance(netlib, use_gpu, method, pivot)
                                 end
-                                
                             end
                         end
                     end
                 end
-
             end
         end
-
     end
 end
